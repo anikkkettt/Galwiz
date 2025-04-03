36c23a57b45353241252fc0187012a0eb12eb5e1
Aniket Dutta <114622119+anikkkettt@users.noreply.github.com>
2025-04-03 06:13:53 +0530

    Update axios.js

--- galwiz-adminPannel/src/api/axios.js
+++ galwiz-adminPannel/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // This baseURL can be changed after deployment
 // Default is the development server URL
 const instance = axios.create({
-  baseURL: 'http://localhost:8082/api/v1',
+  baseURL: 'https://galwiz-backend.onrender.com/api/v1',
 });
 
 export default instance;
